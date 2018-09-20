39fad5e1edaa64f17b1c830325093a1cfe8dab3f
Bruiserjoe <42724327+Bruiserjoe@users.noreply.github.com>
2018-09-19 21:08:16 -0500

    1.0
    Added two commands help and reset

--- Main.cpp
+++ Main.cpp
@@ -19,6 +19,23 @@ int To_Do::mainloop() {
 		load3.printgoals();
 
 	}
+	else if (answer == "help") {
+		std::cout << "Use these three commnds to use the program \n 1. set goal: create a goal \n 2.show goals: show list of goals or to dos \n 3. reset: clear goals and to dos" << std::endl;
+		return mainloop();
+	}
+	else if (answer == "reset") {
+		m_file.close();
+		if (remove("goalfile.txt") != 0) {
+			perror("Error clearing file");
+
+		}
+		else {
+
+			puts("File cleared");
+
+		}
+		return mainloop();
+	}
 	else {
 		std::cout << "That isn't a command try again" << std::endl;
 
